0a6101e7960cce29983434e5002370b514ea5b46
Test <phihung9304@gmail.com>
2026-04-21 17:44:08 +0700

    update

--- pet-managements/routes/web.php
+++ pet-managements/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PetController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\CareGuideController;
 
 // 🏠 Dashboard
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
@@ -14,9 +16,11 @@ Route::resource('customers', CustomerController::class);
 // 🐶 Pets
 Route::resource('pets', PetController::class);
 
+// 🛠 Services
+Route::resource('services', ServiceController::class);
 
+// 📖 Care Guides (Cách nuôi)
+Route::resource('care-guides', CareGuideController::class);
 
 // 👨‍💼 Employees (view tĩnh)
-Route::get('/employees', function () {
-    return view('employees.index');
-});
+Route::view('/employees', 'employees.index')->name('employees.index');
